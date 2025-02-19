e356585c518435931bf922db44943eb8f76e58be
Grougie <144916781+Grougie@users.noreply.github.com>
2025-02-19 15:16:12 +0100

    add profile page

--- symfonyproject/src/Controller/ProfileController.php
+++ symfonyproject/src/Controller/ProfileController.php
@@ -2,13 +2,33 @@
 
 namespace App\Controller;
 
+use Doctrine\Common\Lexer\Token;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use App\Entity\User;
 
 class ProfileController extends AbstractController
 {
-    public function profile()
+    public function profile(TokenStorageInterface $tokenStorage): Response
     {
-        return $this->render('user/profile.html.twig',[]);
+        $token = $tokenStorage->getToken();
+        if (null === $token) {
+            throw new \LogicException('No token found in storage.');
+        }
+
+        $user = $token->getUser();
+        if (!$user instanceof User) {
+            throw new \LogicException('The user is not authenticated or is not an instance of User.');
+        }
+
+        $profilepicture = $user->getProfilePicture();
+        $pseudo = $user->getPseudo();
+        $email = $user->getEmail();
+
+        return $this->render('user/profile.html.twig',[
+            'pseudo' => $pseudo,
+            'email' => $email
+        ]);
     }
 }
