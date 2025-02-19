b8245aee15d9fd93596cb753d929c0980679a89c
lechatn <noe.lechat@ynov.com>
2025-02-19 11:27:48 +0100

    Page group is ok

--- symfonyproject/src/Controller/GroupController.php
+++ symfonyproject/src/Controller/GroupController.php
@@ -9,6 +9,7 @@ use App\Entity\Group;
 use App\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\GroupeCreationType;
+use Doctrine\Common\Lexer\Token;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
@@ -43,13 +44,85 @@ class GroupController extends AbstractController
             $entityManager->flush();
         }
 
+        $groupMembers = $managerRegistry->getRepository(User::class)->findBy(['idGroup' => $user->getIdGroup()]);
+
+        $groupScore = $user->getIdGroup() !== null ? $user->getIdGroup()->getScore() : null;
+
+        $groups = $managerRegistry->getRepository(Group::class)->findAll();
+
+        $groupName = $user->getIdGroup() !== null ? $user->getIdGroup()->getName() : null;
+
         return $this->render('group/group.html.twig', [
             'formGroup' => $form->createView(),
+            'isInGroup' => $user->getIdGroup() !== null,
+            'groupMembers' => $groupMembers,
+            'groupScore' => $groupScore,
+            'groups' => $groups,
+            'groupName' => $groupName,
         ]);
     }
 
-    public function group()
+    public function group(TokenStorageInterface $tokenStorage): Response
     {
-        return $this->render('group/group.html.twig',[]);
+        $token = $tokenStorage->getToken();
+        if (null === $token) {
+            throw new \LogicException('No token found in storage.');
+        }
+
+        $user = $token->getUser();
+        if (!$user instanceof User) {
+            throw new \LogicException('The user is not authenticated or is not an instance of User.');
+        }
+
+        return $this->render('group/group.html.twig',[
+            'isInGroup' => $user->getIdGroup() !== null,
+        ]);
     }
+
+    #[Route('/group/leave', name: 'leave_group')]
+    public function leaveGroup(TokenStorageInterface $tokenStorage, ManagerRegistry $managerRegistry): Response
+    {
+        $token = $tokenStorage->getToken();
+        if (null === $token) {
+            throw new \LogicException('No token found in storage.');
+        }
+
+        $user = $token->getUser();
+        if (!$user instanceof User) {
+            throw new \LogicException('The user is not authenticated or is not an instance of User.');
+        }
+
+        $user->setIdGroup(null);
+
+        $entityManager = $managerRegistry->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('group');
+    }
+
+    #[Route('/group/join/{id}', name: 'join_group')]
+    public function joinGroup(TokenStorageInterface $tokenStorage, ManagerRegistry $managerRegistry, $id): Response
+    {
+        $token = $tokenStorage->getToken();
+        if (null === $token) {
+            throw new \LogicException('No token found in storage.');
+        }
+
+        $user = $token->getUser();
+        if (!$user instanceof User) {
+            throw new \LogicException('The user is not authenticated or is not an instance of User.');
+        }
+
+        $group = $managerRegistry->getRepository(Group::class)->find($id);
+
+        $user->setIdGroup($group);
+
+        $entityManager = $managerRegistry->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('group');
+    }
+
 }
